6a17bf96c6fc0624bb18818d9f8b90076eec2128
FROSTYLAN <castillo089frosty@gmail.com>
2022-09-02 09:50:19 -0500

    search

--- 05_Algoritmos/List.js
+++ 05_Algoritmos/List.js
@@ -26,9 +26,52 @@ class List {
     this.head = null;
   }
 
-  add(value) {
-    const node = new Node(value);
-    node.setNext(this.head);
-    this.head = node;
+  search(data) {
+    const current = this.head;
+    const found = false;
+
+    while (current && !found) {
+      if (current.getData() === data) {
+        found = true;
+      }
+    }
+  }
+
+  remove(data) {
+    let current = this.head;
+    let previous = null;
+    let found = false;
+
+    while (!found) {
+      if (current.getData() === data) {
+        found = true;
+      } else {
+        previous = current;
+        current = current.getNext();
+      }
+    }
+
+    if (previous === null) {
+      this.head = current.getNext();
+    } else {
+      previous.setNext(current.getNext());
+    }
+  }
+
+  size() {
+    let current = this.head;
+    let counter = 0;
+
+    while (current != null) {
+      counter++;
+      current = current.getNext();
+    }
+
+    return counter;
+    a;
+  }
+
+  isEmpty() {
+    return this.head === null;
   }
 }
